e1bab19d8c0189d47d296b9c74becaeb4f5d9949
Boris Yankov <borisyankov@gmail.com>
2015-01-16 18:31:33 +0200

    Refactor daily report code

--- code.js
+++ code.js
@@ -22,14 +22,6 @@ $(function() {
 
     $('#language_select select').on('change', function() {
 
-        /*var url = window.location.href,
-            parser = document.createElement('a'),
-            lang = $('#language_select select option:selected').val(),
-            lastIdx = langCodeIdx(url),
-            codeInUrl = url.substring(lastIdx + 1, lastIdx + 3),
-            langOffset = isLangCode(codeInUrl) ? 3 : 0,
-            newUrl2 = url.substring(0, lastIdx + 1) + (lang == 'en' ? '' : (lang + '-')) + url.substring(lastIdx + 1 + langOffset);*/
-
         var lang = $(this).val();
 
         window.location.href = '/' + (lang == 'en' ? '' : lang);
@@ -53,14 +45,8 @@ $(function() {
 
     $('.wcustomhtml').css('overflow', 'visible');
 
-    function getDailyReport() {
-        $.get('//js.binary.com/javascript.php', {
-                prefix: 'bPzDzniJKAJHH6eEtUVc2GNd7ZgqdRLk',
-                media: '26',
-                campaign: '1',
-               mode: 'txt'
-            },
-           function(xml) {
+    function getDailyReport(options) {
+        $.get('//js.binary.com/javascript.php', options, function(xml) {
                 var content = '';
                 var recent = '';
 
@@ -77,7 +63,7 @@ $(function() {
                         .replace(']]>', '')
                        .replace(/>(.*?)</, '');
 
-                    post = post.substr(post.indexOf('<div>') + 5, post.lastIndexOf('</div>') - 6);
+                    //post = post.substr(post.indexOf('<div>') + 5, post.lastIndexOf('</div>') - 6);
 
                     $('.report-list').append($('<option>', {
                         value: idx,
@@ -88,16 +74,31 @@ $(function() {
                         .append('<h1>' + title + '</h1>')
                         .append('<span class="post-meta">' + pubDate + '</span>')
                         .append('<p>' + post + '</p>')
-                       .toggle(idx == 0)
+                        .toggle(idx == 0)
                         .appendTo('.daily-report');
                 });
             },
             'xml'
         );
-     }
+    }
 
     if ($('.daily-report').length) {
-        getDailyReport();
+        getDailyReport({
+            prefix: 'bPzDzniJKAJHH6eEtUVc2GNd7ZgqdRLk',
+            media: '26',
+            campaign: '1',
+            mode: 'txt'
+        });
+    }
+
+    // daily Report Russuian feed==================http://js.binary.com/javascript.php?prefix=bPzDzniJKAJM5vlemjwB2mNd7ZgqdRLk&media=875&campaign=1&mode=txt
+    if ($('.daily-report-ru').length) {
+        getDailyReport({
+            prefix: 'bPzDzniJKAJM5vlemjwB2mNd7ZgqdRLk',
+            media: '875',
+            campaign: '1',
+            mode: 'txt'
+        });
     }
 
     $('.report-list').on('change', function(ev) {
@@ -137,53 +138,6 @@ $(function() {
             scrollTop: $(".video-container").offset().top
         }, 300);
     });
-    // daily Report Russuian feed==================http://js.binary.com/javascript.php?prefix=bPzDzniJKAJM5vlemjwB2mNd7ZgqdRLk&media=875&campaign=1&mode=txt
-    function getDailyReportRu() {
-        $.get('//js.binary.com/javascript.php', {
-                prefix: 'bPzDzniJKAJM5vlemjwB2mNd7ZgqdRLk',
-                media: '875',
-                campaign: '1',
-               mode: 'txt'
-            },
-           function(xml) {
-                var content = '';
-                var recent = '';
-
-               $(xml).find('item').each(function(idx) {
-
-                    var title = $(this).find('title').text().replace(/regentmarkets\d.*$/, ''),
-                        pubDate = $(this).find('pubDate').text().replace(/\+0000$/, 'GMT');
-
-                    var post = $(this).find('content\\:encoded').text();
-                    if (!post) {
-                        post = $(this).find('encoded').text();
-                    }
-                    post = post.replace(']]&gt;', '')
-                        .replace(']]>', '')
-                       .replace(/>(.*?)</, '');
-
-                    post = post.substr(post.indexOf('<div>') + 5, post.lastIndexOf('</div>') - 6);
-
-                    $('.report-list').append($('<option>', {
-                        value: idx,
-                        text: title
-                    }));
-
-                    $('<div id="report-' + idx + '" class="single-report">')
-                        .append('<h1>' + title + '</h1>')
-                        .append('<span class="post-meta">' + pubDate + '</span>')
-                        .append('<p>' + post + '</p>')
-                       .toggle(idx == 0)
-                        .appendTo('.daily-report-ru');
-                });
-            },
-            'xml'
-        );
-     }
-
-    if ($('.daily-report-ru').length) {
-        getDailyReportRu();
-    }
 
     $('.report-list').on('change', function(ev) {
         $('.single-report').hide();
