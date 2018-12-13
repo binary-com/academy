b4e8169d1dd5bf99a77526feb358eb022a8b1f1f
Sash <sash@binary.com>
2018-12-13 23:06:41 +0200

    update social widgets

--- js/code.js
+++ js/code.js
@@ -7,16 +7,7 @@ $(function() {
     $(".nav-button").click(function() {
         $(".nav-button,.primary-nav").toggleClass("open");
     });
-    ! function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0],
-            p = /^http:/.test(d.location) ? 'http' : 'https';
-        if (!d.getElementById(id)) {
-            js = d.createElement(s);
-            js.id = id;
-            js.src = p + "://platform.twitter.com/widgets.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }
-    }(document, "script", "twitter-wjs");
+    
     $('.wcustomhtml').css('overflow', 'visible');
 
     function getDailyReport(media, prefix) {
@@ -96,3 +87,4 @@ $(function() {
         }, 300);
     });
 });
+
